e426a4c4815ca9d64101714cc055baec4c2d4e51
McSiMusic <mcsimusic@gmail.com>
2022-07-20 01:06:42 +0700

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BrowserComponent } from './pages/browser/browser.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { PlayerComponent } from './pages/player/player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'player', component: PlayerComponent },
+  { path: 'browser', component: BrowserComponent },
+  { path: 'editor', component: EditorComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PlayerComponent } from './pages/player/player.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BrowserComponent } from './pages/browser/browser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlayerComponent,
+    EditorComponent,
+    AboutComponent,
+    BrowserComponent
   ],
   imports: [
     BrowserModule,
