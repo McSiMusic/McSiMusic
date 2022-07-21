30964802194e90c1a2990ced0faa9b26f559bcd4
McSiMusic <mcsimusic@gmail.com>
2022-07-21 15:40:21 +0700

    back and front

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
+import cors from "cors";
 import { config } from "dotenv";
 
 config();
 
 const app = express();
+app.use(cors({ origin: process.env.FRONTEND }));
+
 app.get("/", (req, res) => {
   console.log("hi");
   res.send("Hello World!");
 });
 
 app.listen(
-  parseInt(process.env.PORT || "8082"),
+  parseInt(process.env.PORT || "5000"),
   process.env.HOST || "localhost",
   () => {
     console.log("started");
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  test$: Observable<any>;
+  constructor(private _http: HttpClient) {
+    this.test$ = this._http.get(environment.baseUrl, { responseType: 'text' });
+  }
 }
